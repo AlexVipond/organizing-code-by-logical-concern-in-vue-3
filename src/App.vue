44f681ec59f792f6a1cb7023032c0e6b0d3d4ad9
AlexVipond <alex.r.vipond@gmail.com>
2021-10-30 14:43:57 -0400

    sketch basic state and keyboard interactions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <main class="h-screen w-screen flex items-center justify-center bg-gray-900">
-    <h1 class="text-2xl tracking-widest font-bold uppercase text-gray-400">Organizing Code by Logical Concern in Vue 3</h1>
+  <main class="
+    h-screen w-screen
+    flex flex-col gap-10 p-6
+    bg-gray-100 text-gray-900
+  ">
+
+    <section class="mx-auto w-full max-w-sm flex flex-col gap-4 items-center justify-center h-full">
+      <label class="w-full text-sm italic tracking-wide text-gray-500">Choose an option:</label>
+      <BaseListbox :options="options" v-model="selected"></BaseListbox>
+    </section>
+    <h1 class="text-xs tracking-widest text-center italic text-gray-500">Organizing Code by Logical Concern in Vue 3</h1>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref, shallowRef } from 'vue'
+import { options as o } from './options'
+import BaseListbox from './BaseListbox.vue'
 
+const options = shallowRef(o)
+
+const selected = ref(options.value[0])
 </script>
