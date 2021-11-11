c053ae09c75da7518211a7753cc9cedac87aa7c3
AlexVipond <alex.r.vipond@gmail.com>
2021-11-11 12:42:13 -0600

    more end slide

--- src/components/SlideEnd.vue
+++ src/components/SlideEnd.vue
@@ -6,10 +6,20 @@
       <a
         href="https://talks.alexvipond.dev"
         rel="noopener noreferrer"
-        class="mt-6 text-6xl font-bold text-primary-600 hover:underline"
+        class="mt-4 text-6xl font-bold text-primary-600 hover:underline"
       >talks.alexvipond.dev</a>
     </h2>
-    <section class="grid grid-cols-1 pt-12 gap-20">
+    <p class="mt-5 flex items-center gap-10">
+      <a
+        href="https://twitter.com/AlPalVipond"
+        rel="noopener noreferrer"
+        class="text-2xl flex items-center gap-2 text-primary-600 hover:underline"
+      >
+        <svg class="fill-current h-[1em] w-[1em]" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/></svg>
+        @AlPalVipond
+      </a>
+    </p>
+    <section class="mt-12 grid grid-cols-1 gap-20">
       <section class="flex flex-col gap-6 max-w-md text-xl">
         <p class="flex items-start gap-2">
           <!-- TODO: link -->
